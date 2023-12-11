675d144fda17f098b50907dafd2f91c7b3987e5e
mendes-low <uskembaev07@icloud.com>
2023-12-11 22:08:29 +0600

    lesson-1_variables.1

---
+++ variables-11.12.23/index.js
@@ -0,0 +1,36 @@
+// console.log('Hello world')
+// console.log('Im 16 year old')
+// console.log('Im from Almaty')
+// console.log(16)
+// console.log(true)
+// console.log(1.50)
+
+// let first = 'Aziz'
+// let lastname = 'Uskembaev'
+// let age = 16
+// let city = 'Almaty'
+// let country = `Kazakhstan`
+// console.log(first, lastname, age, city, city, country)
+
+// console.log(`My name is ${first} ${lastname}. Im ${age} years old. I live ${city}. Im from ${country}`)
+
+// let five = 5 
+// let six = 6 
+// console.log(`${five}*${six}=${five*six}`)
+
+// let a = 4536 
+// let b = 237 
+// let c = 485 
+// let d = 123
+// console.log(`${a}*${b}+${c}*${d}=${a*b+c*d}`)
+
+// const name1 = 'Aziz'
+// let last = 'Aslan'
+// last = 'Sultan'
+// console.log(last);
+// console.log(name1);
+
+
+
+
+
